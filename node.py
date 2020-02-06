67ca4c38c3a1d1dc16180b5b3c162757da69e40e
Jonathan Hsu <bit172@gmail.com>
2020-02-05 21:47:18 -0500

    Implemented find_children method

--- node.py
+++ node.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 
+def stringify(board):
+    s = ""
+    for i in range(Node.n):
+        for j in range(Node.n):
+            s += str(board[i, j])
+    return s
+
+
 class Node:
     n = 0
 
@@ -13,8 +21,11 @@ class Node:
                 board[i, j] = initial[index]
                 index += 1
         self.v = board
+        self.touched = None
 
     def find_children(self):
+        dt = np.dtype([('child', np.unicode_, Node.n ** 2), ('touched', np.unicode_, 2)])
+        string_children = np.empty([Node.n ** 2], dtype=dt)
         children = np.empty([Node.n ** 2], dtype=object)
         index = 0
         for i in range(Node.n):
@@ -22,6 +33,7 @@ class Node:
                 # copy the current state of the board
                 child = np.array(self.v)
                 # change the touched token
+                touched = str(i) + str(j)
                 if child[i, j]:
                     child[i, j] = 0
                 else:
@@ -33,7 +45,7 @@ class Node:
                     else:
                         child[i, j - 1] = 1
                 # right
-                if j + 1 > Node.n - 1:
+                if j + 1 <= Node.n - 1:
                     if child[i, j + 1]:
                         child[i, j + 1] = 0
                     else:
@@ -45,8 +57,17 @@ class Node:
                     else:
                         child[i - 1, j] = 1
                 # bottom
-                if i + 1 > Node.n - 1:
+                if i + 1 <= Node.n - 1:
                     if child[i + 1, j]:
                         child[i + 1, j] = 0
                     else:
                         child[i + 1, j] = 1
+                string_children[index]["child"] = stringify(child)
+                string_children[index]["touched"] = touched
+                index += 1
+        # sort the children
+        string_children = np.sort(string_children)
+        for idx, string_child in enumerate(string_children):
+            children[idx] = Node(Node.n, string_child["child"])
+            children[idx].touched = string_child["touched"]
+        return children
