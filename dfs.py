5a672877782b42bdf127b7ef3c967235e3a936d8
bit172 <bit172@gmail.com>
2020-02-07 16:07:48 -0500

    Added search and solution file outputs

--- dfs.py
+++ dfs.py
@@ -1,40 +1,34 @@
 import numpy as np
 from node import Node
 
-f = open('input.txt', "r")
-# Converts all inputs into ints except the last one
-params = [int(val) if idx < 2 else val for idx, val in enumerate(f.read().split(" "))]
+f1 = open('input.txt', "r")
 
-initial_board = Node(params[0], params[2])
-goal = np.zeros((params[0], params[0]), dtype=np.uint8)
-max_d = params[1]
+puzzles = f1.readlines()
+input_puzzles = []
+for puzzle in puzzles:
+    # Converts all inputs into ints except the last one
+    input_puzzle = [int(val) if idx < 2 else val for idx, val in enumerate(puzzle.strip().split(" "))]
+    input_puzzles.append(input_puzzle)
 
-
-def exists_in_closed(closed_list, current):
-    """
-    Checks if a board state exists in the closed_list
-
-    :param closed_list: closed list
-    :param current: board to test
-    :return: if the board is in the closed list
-    """
-
-    exists = False
-    for b in closed_list:
-        if np.array_equal(b, current):
-            exists = True
-            break
-    return exists
+f1.close()
 
 
-def dfs():
+def touched_to_string(n):
+    return str(chr(int(n[0]) + 65)) + n[1]
+
+
+def dfs(params, search_file):
+    initial_board = Node(params[0], params[2])
+    goal = np.zeros((params[0], params[0]), dtype=np.uint8)
+    max_d = params[1]
     # Open Stack with elements like (current board value, parent, depth)
     open_stack = [(initial_board, 1)]
-    closed_list = set()
+    closed_set = set()
 
     while open_stack:
         current = open_stack.pop()
-        closed_list.add(current[0].string_v)
+        closed_set.add(current[0].string_v)
+        search_file.write("0 0 0 " + current[0].string_v + "\n")
         depth = current[1] + 1
 
         if np.array_equal(current[0].v, goal):
@@ -43,17 +37,30 @@ def dfs():
         if depth <= max_d:
             children = current[0].find_children()
             for child in children:
-                if child.string_v not in closed_list:
+                if child.string_v not in closed_set:
                     child.p = current[0]
                     open_stack.append((child, depth))
 
 
 if __name__ == '__main__':
-    sol_node = dfs()
-    parent = sol_node
-    if not parent:
-        print("no solution")
-    while parent:
-        print(parent.v)
-        print(parent.touched)
-        parent = parent.p
+    count = 0
+    for i in input_puzzles:
+        f1 = open(f"{count}_dfs_solution.txt", "w")
+        f2 = open(f"{count}_dfs_search.txt", "w")
+        solution = dfs(i, f2)
+        f2.close()
+        if not solution:
+            f1.write("no solution")
+            continue
+        parent = solution
+        solution_path = []
+        while parent:
+            solution_path.append(parent)
+            parent = parent.p
+        node = solution_path.pop()
+        f1.write("0 " + node.string_v + "\n")
+        while solution_path:
+            node = solution_path.pop()
+            f1.write(touched_to_string(node.touched) + " " + node.string_v + "\n")
+        count += 1
+        f1.close()
