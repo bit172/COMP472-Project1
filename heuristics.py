faa6c662311deecc1cb268cd7298a71627c6e59a
ys-lin <dabris16.x@gmail.com>
2020-02-26 18:43:44 -0500

    even more magical formula

--- heuristics.py
+++ heuristics.py
@@ -14,8 +14,8 @@ def h2(board, n):
         for j in range(n):
             if board[i, j]:
                 totalBlack += 1
-            if is_pattern(board, n, i, j):
-                part_of_pattern += tokens_covered(board, n, i, j)
+                if is_pattern(board, n, i, j):
+                    part_of_pattern += tokens_covered(board, n, i, j)
     return totalBlack + abs(totalBlack - part_of_pattern)
 
 
@@ -29,7 +29,7 @@ def is_pattern(board, n, i, j):
 
 
 def tokens_covered(board, n, i, j):
-    counter = 0
+    counter = 1
     if i + 1 < n:
         if board[i + 1, j]:
             counter += 1
